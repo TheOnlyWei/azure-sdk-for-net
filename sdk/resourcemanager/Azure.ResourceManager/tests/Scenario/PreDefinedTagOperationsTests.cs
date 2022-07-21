bf8438a52dde61f76caf08cff64b123a988a543c
Wei Shi <weishi9999@gmail.com>
2022-07-21 15:55:43 -0700

    update PreDefinedTagOperationsTests for Azure Stack API version

--- sdk/resourcemanager/Azure.ResourceManager/tests/Scenario/PreDefinedTagOperationsTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager/tests/Scenario/PreDefinedTagOperationsTests.cs
@@ -11,13 +11,14 @@ using NUnit.Framework;
 
 namespace Azure.ResourceManager.Tests
 {
+    [ClientTestFixture(true, "2021-04-01", "2016-06-01")]
     public class PredefinedTagOperationsTests : ResourceManagerTestBase
     {
         private string _tagName;
         private SubscriptionResource _subscription;
 
-        public PredefinedTagOperationsTests(bool isAsync)
-            : base(isAsync)//, RecordedTestMode.Record)
+        public PredefinedTagOperationsTests(bool isAsync, string apiVersion)
+            : base(isAsync, SubscriptionResource.ResourceType, apiVersion)//, RecordedTestMode.Record)
         {
         }
 
