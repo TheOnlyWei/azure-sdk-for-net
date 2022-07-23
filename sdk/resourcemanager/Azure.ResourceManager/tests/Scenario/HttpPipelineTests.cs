a9a6199db65c07f093295eb8dcf064ba1e10f932
Wei Shi <weishi9999@gmail.com>
2022-07-22 18:51:40 -0700

    update HttpPipelineTests for Azure Stack API version

--- sdk/resourcemanager/Azure.ResourceManager/tests/Scenario/HttpPipelineTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager/tests/Scenario/HttpPipelineTests.cs
@@ -7,14 +7,15 @@ using NUnit.Framework;
 
 namespace Azure.ResourceManager.Tests
 {
+    [ClientTestFixture(true, "2021-04-01", "2016-06-01")]
     [Parallelizable]
     public class HttpPipelineTests : ResourceManagerTestBase
     {
         private ArmClient _client;
         private string _rgName;
 
-        public HttpPipelineTests(bool isAsync)
-            : base(isAsync) //, RecordedTestMode.Record)
+        public HttpPipelineTests(bool isAsync, string apiVersion)
+            : base(isAsync, SubscriptionResource.ResourceType, apiVersion) //, RecordedTestMode.Record)
         {
         }
 
