86d0ff6637d9fffa2f2a883158ece5c325372fe8
Wei Shi <weishi9999@gmail.com>
2022-07-22 20:29:53 -0700

    update ArmBuilderTests for Azure Stack API version

--- sdk/resourcemanager/Azure.ResourceManager/tests/Scenario/ArmBuilderTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager/tests/Scenario/ArmBuilderTests.cs
@@ -7,10 +7,11 @@ using NUnit.Framework;
 
 namespace Azure.ResourceManager.Tests
 {
+    [ClientTestFixture(true, "2021-04-01", "2016-06-01")]
     public class ArmBuilderTests : ResourceManagerTestBase
     {
-        public ArmBuilderTests(bool isAsync)
-            : base(isAsync)//, RecordedTestMode.Record)
+        public ArmBuilderTests(bool isAsync, string apiVersion)
+            : base(isAsync, SubscriptionResource.ResourceType, apiVersion)//, RecordedTestMode.Record)
         {
         }
 
